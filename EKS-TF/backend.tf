b6264b221dbdb35461d3fa67170c2918073abee9
Gerardbulky <gerardambe@yahoo.com>
2025-03-09 12:06:27 +0100

    Changes bucket key

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-bakett2"
+    bucket         = "my-bakett22"
     region         = "us-east-1"
     key            = "End-to-End-Kubernetes-DevSecOps-Resume-Project/EKS-TF/terraform.tfstate"
     encrypt        = true
--- Jenkins-Terraform-Infra/backend.tf
+++ Jenkins-Terraform-Infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-bakett2"
+    bucket         = "my-bakett22"
     region         = "us-east-1"
     key            = "End-to-End-Kubernetes-DevSecOps-Resume-Project/Jenkins-Terraform-Infra/terraform.tfstate"
     encrypt        = true
