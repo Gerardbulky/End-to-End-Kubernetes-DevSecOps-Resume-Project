d107f9573fac06bdd622ba4082994c83a91918c3
Gerardbulky <gerardambe@yahoo.com>
2025-03-14 09:32:03 +0100

    Modified Jenkins-Terraform-Infra/ec2.tf

--- Jenkins-Terraform-Infra/keypair.tf
+++ Jenkins-Terraform-Infra/keypair.tf
@@ -1,4 +1,14 @@
-resource "aws_key_pair" "jenkins" {
-  key_name   = var.key-name
-  public_key = file("/home/ubuntu/.ssh/Jenkins-key.pub")
+resource "tls_private_key" "my_rsa_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "my_key_pair" {
+  key_name   = var.Jenkins-key
+  public_key = tls_private_key.my_rsa_key.public_key_openssh
+}
+
+resource "local_file" "my_private_key" {
+  content  = tls_private_key.my_rsa_key.private_key_pem
+  filename = var.Jenkins_private_key_file
 }
